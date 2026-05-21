33d0c9a015ccfc5d5ed8cb63199d7528c7302da9
simon cadavid <simon.cadavid98@gmail.com>
2026-05-22 00:57:08 +0200

    feat(Solvers): interior dirichlet and neumann, direct and indirect, api for geometry and also precomputed operators

--- src/Solvers.jl
+++ src/Solvers.jl
@@ -12,14 +12,88 @@ using ..Manifolds
 export solve
 
 
+
+# api to solve with given precomputed operators
+function solve(
+    ::Laplace,
+    ::Interior,
+    bc::Dirichlet,
+    ::Direct,
+    D_star::AdjointDoubleLayer,
+    H::Hypersingular,
+    S_target::SingleLayer,
+    D_target::DoubleLayer
+)
+    # TODO: work in place to avoid allocating a new matrix
+    A = -0.5 * I + matrix(D_star) # TODO: figure out how to seamlessly fulfill the matrix api
+    τ = A \ (H * bc.σ)
+    u = S_target * τ - D_target * bc.σ
+    return u, τ
+end
+
+# indirect approach
+function solve(
+    ::Laplace,
+    ::Interior,
+    bc::Dirichlet,
+    ::Indirect,
+    D::DoubleLayer,
+    H::Hypersingular,
+    D_target::DoubleLayer
+)
+
+    η = (-0.5 * I + matrix(D)) \ bc.σ # auxiliary variable
+
+    u = D_target * η
+    τ = H * η
+    return u, τ
+end
+
+function solve(
+    ::Laplace,
+    ::Interior,
+    bc::Neumann,
+    ::Direct,
+    S::SingleLayer,
+    D::DoubleLayer,
+    S_target::SingleLayer,
+    D_target::DoubleLayer,
+)
+    σ = (0.5 * I + matrix(D)) \ (S * bc.τ)
+
+    u = S_target * bc.τ - D_target * σ
+
+    return u, σ
+end
+
+function solve(
+    ::Laplace,
+    ::Interior,
+    bc::Neumann,
+    ::Indirect,
+    S::SingleLayer,
+    D_star::AdjointDoubleLayer,
+    S_target::SingleLayer,
+)
+    η = (0.5 * I + matrix(D_star)) \ bc.τ
+
+    u = S_target * η
+
+    σ = S * η
+
+    return u, σ
+end
+
+
+# api to compute operators internally
 function solve(
     ::BoundaryValueProblem,
     ::Side,
-    ::AbstractManifold,
     ::BoundaryCondition,
+    targets::AbstractMatrix, # by convention, k(x, y), so targets go first
+    ::AbstractManifold,
     ::HypersingularCorrection,
     ::Approach,
-    targets::AbstractMatrix
 )
 
 end
@@ -27,11 +101,11 @@ end
 function solve(
     problem::Laplace,
     side::Interior,
-    boundary::AbstractManifold,
     bc::Dirichlet,
+    targets::AbstractMatrix,
+    boundary::AbstractManifold,
     correction::HypersingularCorrection,
     approach::Direct,
-    targets::AbstractMatrix
 )
     # operators with quadrature weights applied
     # TODO: how to save computation by getting both operators at the same time
@@ -54,51 +128,82 @@ function solve(
 
 end
 
-
-# solve with given precomputed operators
 function solve(
-    ::Laplace,
-    ::Interior,
+    problem::Laplace,
+    side::Interior,
     bc::Dirichlet,
-    ::Direct,
-    D_star::AdjointDoubleLayer,
-    H::Hypersingular,
-    S_target::SingleLayer,
-    D_target::DoubleLayer
+    targets::AbstractMatrix,
+    boundary::AbstractManifold,
+    correction::HypersingularCorrection,
+    approach::Indirect,
 )
-    A = -0.5 * I + matrix(D_star) # TODO: figure out how to seamlessly fulfill the matrix api
-    τ = A \ (H * bc.σ)
-    u = S_target * τ - D_target * bc.σ
-    return u, τ
+
+    D = DoubleLayer(problem, boundary)
+    H = Hypersingular(problem, boundary, correction)
+    D_target = DoubleLayer(problem, targets, boundary)
+
+    return solve(
+        problem,
+        side,
+        bc,
+        approach,
+        D,
+        H,
+        D_target,
+    )
 end
+
 
 function solve(
     problem::Laplace,
-    type::Interior,
+    side::Interior,
+    targets::AbstractMatrix,
+    boundary::AbstractManifold,
     bc::Neumann,
     approach::Direct,
-    targets::AbstractMatrix
 )
 
+    S = SingleLayer(problem, boundary,)
+    D = DoubleLayer(problem, boundary,)
+    S_target = SingleLayer(problem, targets, boundary,)
+    D_target = DoubleLayer(problem, targets, boundary,)
+
+    return solve(
+        problem,
+        side,
+        bc,
+        approach,
+        S,
+        D,
+        S_target,
+        D_target,
+    )
 end
 
 function solve(
     problem::Laplace,
-    type::Interior,
-    bc::Dirichlet,
-    approach::Indirect,
-    targets::AbstractMatrix
-)
-
-end
-
-function solve(
-    problem::Laplace,
-    type::Interior,
+    side::Interior,
+    targets::AbstractMatrix,
+    boundary::AbstractManifold,
     bc::Neumann,
     approach::Indirect,
-    targets::AbstractMatrix
 )
 
+    S = SingleLayer(problem, boundary)
+    D_star = AdjointDoubleLayer(problem, boundary)
+    S_target = SingleLayer(problem, targets, boundary,)
+
+    return solve(
+        problem,
+        side,
+        bc,
+        approach,
+        S,
+        D_star,
+        S_target,
+    )
+
+
 end
+
 end
