d1efa05ce53dd0a6828e2896a3bf0b0d8ca2f437
Simon Cadavid <simon.cadavid@tum.de>
2026-06-16 21:06:11 +0200

    modify testing script for ellipse

--- scripts/ellipse.jl
+++ scripts/ellipse.jl
@@ -51,7 +51,7 @@ function main()
     end
 
     n_grid = 200
-    θ_grid = range(0, 2π; length=n_grid + 1)[1:end-1]
+    θ_grid = range(0, 2π; length=n_grid + 1)[1:(end-1)]
     boundary_grid = Matrix(stack((t) -> rotated_ellipse(t, x0, y0, a, b, γ), θ_grid)')
 
     xmin, ymin = minimum(boundary_grid, dims=1) |> vec
@@ -74,7 +74,7 @@ function main()
         dims=1
     )
 
-    n_vals = 20:80:1000
+    n_vals = 200:80:200
     errs = zeros(Float64, size(n_vals, 1))
 
     ord = 32
@@ -94,7 +94,7 @@ function main()
     for (i, n) in enumerate(n_vals)
 
         # Define boundary
-        θ = range(0, 2π; length=n + 1)[1:end-1]
+        θ = range(0, 2π; length=n + 1)[1:(end-1)]
         Γ = DiscreteClosedCurve(θ, (t) -> rotated_ellipse(t, x0, y0, a, b, γ))
 
 
@@ -111,35 +111,99 @@ function main()
         xi_eta_exact = xi_eta_exact_all[mask, :]
 
         D = DoubleLayer(laplace, Γ)
+        S = SingleLayer(laplace, Γ, ord)
         H_zeta = Hypersingular(laplace, Γ, zeta)
         H_sidi = Hypersingular(laplace, Γ, sidi)
 
         D_target = DoubleLayer(laplace, x_test, Γ,)
+        S_target = SingleLayer(laplace, x_test, Γ,)
 
-        xi, _ = solve(
+        # TODO: work with vector valued functions
+        bc_xi = Dirichlet(cos.(θ))
+        bc_eta = Dirichlet(sin.(θ))
+
+
+        # xi, _ = solve(
+        #     laplace,
+        #     interior,
+        #     bc_xi,
+        #     indirect,
+        #     D,
+        #     H_zeta,
+        #     D_target
+        # )
+        #
+        # eta, _ = solve(
+        #     laplace,
+        #     interior,
+        #     bc_eta,
+        #     indirect,
+        #     D,
+        #     H_zeta,
+        #     D_target
+        # )
+
+        # close evaluation
+
+        phi_xi = solve_bie(
             laplace,
             interior,
-            Dirichlet(cos.(θ)),
-            indirect,
+            bc_xi,
             D,
-            H_zeta,
-            D_target
         )
 
-        eta, _ = solve(
+        phi_eta = solve_bie(
             laplace,
             interior,
-            Dirichlet(sin.(θ)),
-            indirect,
+            bc_eta,
             D,
-            H_zeta,
-            D_target
         )
+
+        v_lim_xi = compute_boundary_limit(
+            interior,
+            D,
+            phi_xi,
+            Γ
+        )
+        v_xi = cauchy_integral(
+            x_test,
+            Γ,
+            v_lim_xi
+        )
+        xi = real.(v_xi)
+
+        v_lim_eta = compute_boundary_limit(
+            interior,
+            D,
+            phi_eta,
+            Γ
+        )
+
+        v_eta = cauchy_integral(
+            x_test,
+            Γ,
+            v_lim_eta
+        )
+        eta = real.(v_eta)
 
         xi_eta_num = hcat(xi, eta)
 
+        # f = Figure()
+        # a = Axis(f[1, 1]; aspect=DataAspect())
+        # scatterlines!(a, real.(v_lim_xi), real.(v_lim_eta); markersize=4)
+        # scatter!(a, xi, eta; markersize=4)
+        # scatter!(a, xi_eta_exact...; markersize=4)
+        # return f
+        # return scatterlines(real.(v_lim_xi), real.(v_lim_eta); markersize=10)
+
         # Nx2
         e = xi_eta_num .- xi_eta_exact
+        # display(xi)
+        # display(eta)
+        # display(xi_eta_num)
+        # display(e)
+
+        # break
 
         # Nx1, store euclidean norm of error for each point
         e_norm = norm.(eachrow(e), 2) .+ eps(Float64)
@@ -147,33 +211,34 @@ function main()
         # 1x1
         errs[i] = mean(e_norm .^ 2)
 
-        fig, ax = visualize(Γ)
+        @show i, errs[i]
 
-        scatter_kwargs = (; colorscale=log10, color=e_norm, markersize=7, colormap=:viridis)
-
-        sc1 = scatter!(ax, x_test[:, 1], x_test[:, 2]; scatter_kwargs...)
-
-        ax2 = Axis(fig[1, 2]; aspect=DataAspect(), title="n = $n")
-
-        lines!(ax2, xi_eta_exact_boundary[:, 1], xi_eta_exact_boundary[:, 2]; color=:black)
-
-
-        sc2 = scatter!(ax2, xi_eta_exact[:, 1], xi_eta_exact[:, 2]; scatter_kwargs...)
-
-        Colorbar(fig[1, 2][1, 3], sc2; label="log10 error inf norm")
-
-
+        # fig, ax = visualize(Γ)
+        #
+        # scatter_kwargs = (; colorscale=log10, color=e_norm, markersize=7, colormap=:viridis)
+        #
+        # sc1 = scatter!(ax, x_test[:, 1], x_test[:, 2]; scatter_kwargs...)
+        #
+        # ax2 = Axis(fig[1, 2]; aspect=DataAspect(), title="n = $n")
+        #
+        # lines!(ax2, xi_eta_exact_boundary[:, 1], xi_eta_exact_boundary[:, 2]; color=:black)
+        #
+        # sc2 = scatter!(ax2, xi_eta_exact[:, 1], xi_eta_exact[:, 2]; scatter_kwargs...)
+        #
+        # Colorbar(fig[1, 2][1, 3], sc2; label="log10 error inf norm")
+        #
         # wait(display(fig))
+        break
     end
+    #
+    # fig3 = Figure()
+    # ax3 = Axis(fig3[1, 1]; xscale=log10, yscale=log10)
+    #
+    # scatterlines!(ax3, n_vals, errs)
+    # lines!(ax3, n_vals, (n_vals ./ n_vals[1]) .^ (-2), label="O(h^-2)")
+    # lines!(ax3, n_vals, (n_vals ./ n_vals[1]) .^ (-1), label="O(h^-1)")
+    # axislegend(ax3)
 
-    fig3 = Figure()
-    ax3 = Axis(fig3[1, 1]; xscale=log10, yscale=log10)
-
-    scatterlines!(ax3, n_vals, errs)
-    lines!(ax3, n_vals, (n_vals./ n_vals[1]) .^ (-2) , label="O(h^-2)")
-    lines!(ax3, n_vals, (n_vals./ n_vals[1]) .^ (-1) , label="O(h^-1)")
-    axislegend(ax3)
-
-    wait(display(fig3))
+    # wait(display(fig3))
 
 end
