f35f1383bacc964f9910eb48bfccaf3eed34e8b0
Simon Cadavid <simon.cadavid@tum.de>
2026-08-03 15:31:42 +0200

    test: map2disc mapping case

---
+++ test/map2disc_ellipse.jl
@@ -0,0 +1,174 @@
+
+using Test
+using Revise
+using LinearAlgebra
+using StaticArrays
+using Statistics
+using PolygonOps
+using GLMakie
+
+
+using BoundaryIntegralEquations
+
+
+function main(viz=false)
+
+    γ = deg2rad(30.)
+    x0 = 2.
+    y0 = 3.
+    a = 5.
+    b = 2.
+
+
+    function rotated_ellipse(θ, x0, y0, a, b, γ)
+
+        mat = SA[
+            cos(θ)*cos(γ) -sin(θ)*sin(γ)
+            cos(θ)*sin(γ) sin(θ)*cos(γ)
+        ]
+        SA[x0, y0] + mat * SA[a, b]
+    end
+
+    """
+        exact_solution(xy)
+
+    compute the xi, eta values for given pair of xy values
+
+    # Arguments
+    - `xy`: two-element array containing the x and y coordinates
+    """
+    function exact_solution(xy, x0, y0, a, b, γ)
+        x, y = xy[1], xy[2]
+        xi = (x - x0) * cos(γ) + (y - y0) * sin(γ)
+        eta = (x - x0) * -sin(γ) + (y - y0) * cos(γ)
+        return SA[xi/a, eta/b]
+    end
+
+    n_grid = 30
+    θ_grid = range(0, 2π; length=n_grid + 1)[1:(end-1)]
+
+    boundary_grid = stack((t) -> rotated_ellipse(t, x0, y0, a, b, γ), θ_grid; dims=2)
+
+    xmin, ymin = minimum(boundary_grid, dims=2) |> vec
+    xmax, ymax = maximum(boundary_grid, dims=2) |> vec
+
+    xs = range(xmin, xmax, length=n_grid)
+    ys = range(ymin, ymax, length=n_grid)
+
+    x_test_all = stack(((x, y),) -> SA[x, y], Iterators.product(xs, ys); dims=2)
+
+    xi_eta_exact_all = stack(
+        (t) -> exact_solution(t, x0, y0, a, b, γ),
+        eachcol(x_test_all);
+        dims=2
+    )
+    xi_eta_exact_boundary = stack(
+        (t) -> exact_solution(t, x0, y0, a, b, γ),
+        eachcol(boundary_grid);
+        dims=2
+    )
+
+
+    n_vals = 20:80:400
+    errs = zeros(Float64, size(n_vals, 1))
+
+    ord = 32
+
+    laplace = Laplace()
+    interior = Interior()
+    zeta = Zeta(ord)
+    sidi = Sidi()
+    indirect = Indirect()
+
+    for (i, n) in enumerate(n_vals)
+
+        # Define boundary
+        θ = range(0, 2π; length=n + 1)[1:(end-1)]
+        Γ = DiscreteClosedCurve(θ, (t) -> rotated_ellipse(t, x0, y0, a, b, γ))
+
+
+        # check which points are inside
+        poly = [[col[1], col[2]] for col in eachcol(Γ.x)]
+        push!(poly, Γ.x[:, 1])
+
+        mask = [
+            inpolygon((x_test_all[1, col], x_test_all[2, col]), poly) == 1
+            for col in axes(x_test_all, 2)
+        ]
+        x_test = x_test_all[:, mask]
+        xi_exact = xi_eta_exact_all[:, mask]
+
+        xi_num, sigma, tau = map2disc(Γ, θ, x_test)
+
+        jac = map2disc_with_jacobian(Γ, θ, x_test)
+
+        jac_expected = stack(
+            map(1:2) do i
+                solution_derivative(
+                    Direct(),
+                    x_test,
+                    Γ.x,
+                    Γ.n,
+                    Γ.w,
+                    sigma[i],
+                    tau[i],
+                )
+
+            end
+            ;
+            dims=2
+        )
+
+        @show size(jac)
+        @show size(jac_expected)
+
+        jac_err = jac - jac_expected
+
+        jac_err_norm = vec(sqrt.(sum(abs2, jac_err; dims=(1, 2))))
+
+        @show size(jac_err_norm)
+
+        e = xi_num .- xi_exact
+
+        # Nx1, store euclidean norm of error for each point
+        e_norm = norm.(eachcol(e), 2) .+ eps(Float64)
+
+        # 1x1
+        errs[i] = maximum(e_norm )
+
+        @show n, errs[i], minimum(jac_err_norm)
+        if viz
+            fig, ax = visualize(Γ)
+            scatter_kwargs = (;
+                colorscale=log10,
+                color=jac_err_norm,
+                markersize=15,
+                colormap=:viridis,
+            )
+            sc1 = scatter!(ax, x_test[1, :], x_test[2, :]; scatter_kwargs...)
+            arr = arrows2d!(ax, x_test[1, :], x_test[2, :], e[1, :], e[2, :]; lengthscale=0.1)
+            ax2 = Axis(fig[1, 2]; aspect=DataAspect(), title="n = $n")
+            lines!(ax2, xi_eta_exact_boundary[1, :], xi_eta_exact_boundary[2, :]; color=:black)
+            sc2 = scatter!(ax2, xi_exact[1, :], xi_exact[2, :]; scatter_kwargs...)
+            Colorbar(fig[1, 2][1, 3], sc2; label="log10 error inf norm")
+            wait(display(fig))
+        end
+    end
+
+    # convergence plot
+    # fig3 = Figure()
+    # ax3 = Axis(fig3[1, 1]; xscale=log10, yscale=log10)
+    #
+    # scatterlines!(ax3, n_vals, errs)
+    # lines!(ax3, n_vals, (n_vals ./ n_vals[1]) .^ (-2), label="O(h^-2)")
+    # lines!(ax3, n_vals, (n_vals ./ n_vals[1]) .^ (-1), label="O(h^-1)")
+    # axislegend(ax3)
+
+    # wait(display(fig3))
+
+end
+
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    main()
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,3 +4,5 @@ using SafeTestsets
 @safetestset "Integral Operators" include("operators.jl")
 
 @safetestset "Close Evaluation" include("close_evaluation.jl")
+
+@safetestset "Map2Disc Ellipse" include("map2disc_ellipse.jl")
