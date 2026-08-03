35a1579ec0a1a7c51ab0c5dc10ae8ce137b6f8b6
Simon Cadavid <simon.cadavid@tum.de>
2026-08-03 15:32:28 +0200

    progress exposing map2disc api

--- src/map2disc.jl
+++ src/map2disc.jl
@@ -8,13 +8,17 @@ function map2disc(
     points::AbstractMatrix,
 )
 
+    laplace=Laplace()
     indirect=Indirect()
-    hypersingular_correction=Sidi()
+    hypersingular_correction=Zeta(16)
     interior=Interior()
 
+    sigma_xi = cos.(boundary_parameter)
+    sigma_eta = sin.(boundary_parameter)
+
     pbs = [
-        BoundaryValueProblem(laplace, Dirichlet(cos.(boundary_parameter)), interior, boundary),
-        BoundaryValueProblem(laplace, Dirichlet(sin.(boundary_parameter)), interior, boundary),
+        BoundaryValueProblem(laplace, Dirichlet(sigma_xi), interior, boundary),
+        BoundaryValueProblem(laplace, Dirichlet(sigma_eta), interior, boundary),
     ]
 
     phis = solve.(
@@ -35,22 +39,47 @@ function map2disc(
     xi = solns[1][1]
     eta = solns[2][1]
 
+    tau_xi = data(solns[1][2])
+    tau_eta = data(solns[2][2])
+
+
     # stack in column-major matrix of 2d point
     xi_eta = permutedims(hcat(xi, eta))
 
-    return xi_eta
+    return xi_eta, (sigma_xi, sigma_eta), (tau_xi, tau_eta)
 end
 
 
-function map2disc_gradient(
+function map2disc_with_jacobian(
     boundary::DiscreteClosedCurve,
     boundary_parameter::AbstractVector, # pass the parameter used, later include this information inside boundary struct
     points::AbstractMatrix,
 )
 
-    d_xi, xi = Enzyme.autodiff(
-        Enzyme.ReverseWithPrimal
+    jac = stack(
+        map(1:2) do i
+            d_points = Enzyme.make_zero(points)
+            d_boundary = Enzyme.make_zero(boundary)
+            d_boundary_parameter = Enzyme.make_zero(boundary_parameter)
+            d_points = Enzyme.make_zero(points)
+            d_points[i, :] .= 1.
 
+            ad = autodiff(
+                Enzyme.set_runtime_activity(ForwardWithPrimal),
+                map2disc,
+                Duplicated(boundary, d_boundary),
+                Duplicated(boundary_parameter, d_boundary_parameter),
+                Duplicated(points, d_points),
+            )
+
+            @show typeof(ad[1][1]), size(ad[1][1])
+            ad[1][1]
+        end
+        ;
+        dims=2
     )
+
+    return jac
+
 
 end
