a980f3da944561d1e3e646c3534323ad282aad0a
Simon Cadavid <simon.cadavid@tum.de>
2026-08-05 16:01:13 +0200

    Enzyme ext: refactor code to compute derivatives

--- ext/BoundaryIntegralEquationsEnzymeExt.jl
+++ ext/BoundaryIntegralEquationsEnzymeExt.jl
@@ -6,104 +6,124 @@ module BoundaryIntegralEquationsEnzymeExt
 using BoundaryIntegralEquations
 using Enzyme
 
+
 # NOTE: further derivatives are needed for faster optimization algs
 
-"""
-    BimDiff.spatial_gradient()
 
-compute the gradient of the solution of a boundary value problem with respect to
-physical space
-
-"""
-function BoundaryIntegralEquations.spatial_gradient(
-    mode::Enzyme.ForwardMode,
+# TODO: include matrix_factory
+function BoundaryIntegralEquations.evaluate(
+    ::WithSpatialDerivativeFwd,
     problem::BoundaryValueProblem,
+    approach::Indirect,
+    correction::HypersingularCorrection,
+    bie_solution::BoundaryDensity,
     target::AbstractMatrix,
-    approach::Approach,
-    correction::Union{SingularCorrection,HypersingularCorrection},
+    # TODO: turn this into an object
+    relative_cutoff=0.05,
 )
 
-    # allocate input shadow memory
-    d_problem = Enzyme.make_zero(problem)
-    dx = Enzyme.make_zero(target)
-    dy = Enzyme.make_zero(target)
+    # TODO: remove magic constant
+    ads = map(1:2) do i
+        dproblem = Enzyme.make_zero(problem)
 
-    dx[1, :] .= 1.;
-    dy[2, :] .= 1.;
+        dbie_solution = Enzyme.make_zero(bie_solution)
 
+        dtarget = Enzyme.make_zero(target)
+        dtarget[i, :] .= 1.;
 
-    fwd1 = autodiff(
-        ForwardWithPrimal,
-        Const(solve_and_evaluate),
-        Duplicated(problem, d_problem),
-        Const(approach),
-        Const(correction),
-        Duplicated(target, dx)
-    )
+        # TODO: figure out how to compute primal only once
+        mode = (i==1 || true) ? ForwardWithPrimal : Forward
 
-    fwd2 = autodiff(
-        ForwardWithPrimal,
-        Const(solve_and_evaluate),
-        Duplicated(problem, d_problem),
-        Const(approach),
-        Const(correction),
-        Duplicated(target, dy)
-    )
+        # TODO: achieve without runtime activity
+        autodiff(
+            set_runtime_activity(mode),
+            evaluate,
+            Duplicated(problem, dproblem),
+            Const(approach),
+            Const(correction),
+            Duplicated(bie_solution, dbie_solution),
+            Duplicated(target, dtarget),
+            Const(relative_cutoff),
+        )
+    end
 
+    # stack components of gradient in 2xN array
+    gradient = stack(map(ad -> ad[1][1], ads); dims=1)
 
+    primal = ads[1][2]
 
-    return [collect(fwd1[1][1]); collect(fwd2[1][1])]
-
-
+    return gradient, primal
 end
 
-function BoundaryIntegralEquations.spatial_gradient(
-    mode::Enzyme.ReverseMode,
-    problem::BoundaryValueProblem,
-    target::AbstractMatrix,
-    approach::Approach,
-    correction::Union{SingularCorrection,HypersingularCorrection},
-)
+# TODO: re-do reverse mode for new api
+# function BoundaryIntegralEquations.spatial_gradient(
+#     mode::Enzyme.ReverseMode,
+#     problem::BoundaryValueProblem,
+#     target::AbstractMatrix,
+#     approach::Approach,
+#     correction::Union{SingularCorrection,HypersingularCorrection},
+# )
+#
+#
+#     d_problem = Enzyme.make_zero(problem)
+#     d_target = Enzyme.make_zero(target)
+#
+#
+#
+#     forward, reverse = autodiff_thunk(
+#         ReverseSplitWithPrimal,
+#         Const{typeof(solve_and_evaluate)},
+#         Duplicated,
+#         Duplicated{typeof(problem)},
+#         Const{typeof(approach)},
+#         Const{typeof(correction)},
+#         Duplicated{typeof(target)},
+#     )
+#
+#
+#     tape, result, shadow_result = forward(
+#         Const(solve_and_evaluate),
+#         Duplicated(problem, d_problem),
+#         Const(approach),
+#         Const(correction),
+#         Duplicated(target, d_target),
+#     )
+#
+#     shadow_result[1].=1.
+#
+#     rev = reverse(
+#         Const(solve_and_evaluate),
+#         Duplicated(problem, d_problem),
+#         Const(approach),
+#         Const(correction),
+#         Duplicated(target, d_target),
+#         tape,
+#     )
+#
+#     return d_target
+#
+# end
 
 
-    d_problem = Enzyme.make_zero(problem)
-    d_target = Enzyme.make_zero(target)
+# function BoundaryIntegralEquations.map2disc(
+#     derivative_request::WithSpatialDerivativeFwd,
+#     boundary::DiscreteClosedCurve,
+#     boundary_parameter::AbstractVector,
+#     points::AbstractMatrix,
+# )
+#
+#     r = map2disc(
+#         derivative_request,
+#         boundary,
+#         boundary_parameter,
+#         points,
+#     )
+#
+#     @show r
+#
+#
+# end
 
-
-
-    forward, reverse = autodiff_thunk(
-        ReverseSplitWithPrimal,
-        Const{typeof(solve_and_evaluate)},
-        Duplicated,
-        Duplicated{typeof(problem)},
-        Const{typeof(approach)},
-        Const{typeof(correction)},
-        Duplicated{typeof(target)},
-    )
-
-
-    tape, result, shadow_result = forward(
-        Const(solve_and_evaluate),
-        Duplicated(problem, d_problem),
-        Const(approach),
-        Const(correction),
-        Duplicated(target, d_target),
-    )
-
-    shadow_result[1].=1.
-
-    rev = reverse(
-        Const(solve_and_evaluate),
-        Duplicated(problem, d_problem),
-        Const(approach),
-        Const(correction),
-        Duplicated(target, d_target),
-        tape,
-    )
-
-    return d_target
-
-end
 
 end
 
