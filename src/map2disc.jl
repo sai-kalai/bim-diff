92ab360be037a211a1779459e924a47a4b2b7faa
Simon Cadavid <simon.cadavid@tum.de>
2026-08-18 14:05:43 +0200

    feat: newton search for inverse mapping

--- src/map2disc.jl
+++ src/map2disc.jl
@@ -67,3 +67,70 @@ function map2disc(
     end
 end
 
+
+@doc raw"""
+    inverse_map2disc(boundary::DiscreteClosedCurve, boundary_parameter::AbstractVector, # pass the parameter used, later include this information inside boundary struct, xi_point::AbstractMatrix, relative_cutoff=0.05)
+
+find the corresponding cartesian coordinates for the requested logical coordinates
+
+# Arguments
+- `boundary::DiscreteClosedCurve`: boundary of the mapping
+- `boundary_parameter::AbstractVector`: parameter of the boundary
+- `xi_point::AbstractMatrix`: logical point whose cartesian coordinate is needed
+"""
+function inverse_map2disc(
+    boundary::DiscreteClosedCurve,
+    boundary_parameter::AbstractVector, # pass the parameter used, later include this information inside boundary struct
+    xi_query,
+    relative_cutoff,
+    x0,
+)
+
+    x = newton_search(
+        u -> begin
+            jac, xi, _ = map2disc(
+                WithSpatialDerivativeFwd(),
+                boundary,
+                boundary_parameter,
+                u,
+                relative_cutoff
+            )
+            # jacobian                        residual
+            transpose(reshape(jac, (2, 2))), (xi - xi_query)
+        end,
+        x0,
+        100,
+        1e-5,
+    )
+
+    return x
+end
+
+function newton_search(func, u0, max_iter, tol)
+    res = Inf
+
+    u = u0  # initial guess for x
+    i = 0
+
+    while norm(res) > tol
+
+        jac, res = func(u)
+
+        pb = LinearProblem(jac, res)
+
+        Δu = LinearSolve.solve(pb)
+
+        @show i, u, norm(res)
+
+        u = u - Δu
+
+        i+=1
+
+        if i >= max_iter
+            error("maximum iterations reached")
+        end
+
+    end
+
+    return u
+end
